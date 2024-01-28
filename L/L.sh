106de4935991bc80d316ddd87094da1c9232a6ac
LegrosGabriel <133345446+LegrosGabriel@users.noreply.github.com>
2024-01-28 18:03:55 +0100

    Update L.sh commentaire
    ajout de commentaires aux programmes L.sh

--- L/L.sh
+++ L/L.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-
-start=$(echo $SECONDS );
+#Sert a compter le nombre de secondes de l'execution du programme
+start=$(echo $SECONDS ); #Recupere le nombre de seconde a partir duquel le terminal a ete ouvert
 
 fichier="$1"
 echo "--- chargement en cours du traitement L ---"
@@ -19,44 +19,45 @@ echo "██▒▒▒▒▒▒▒▒ 20%"
 LC_NUMERIC=C awk -F';' 'NR>1 {distances[$1] += $5 } END { for (i in distances) printf "%s;%.2f\n", i, distances[i] }' $fichier | LC_NUMERIC=C sort -t';' -k2,2nr | head -n 10 | LC_NUMERIC=C sort -t ';' -k1n > demo/L.txt
 
 echo "████████▒▒ 80%"
-
-
-
-
-
-
-
-
-
-
+#___________________________________________________________________________________________
+#                                         GNUPLOT
+#___________________________________________________________________________________________
 # Création d'un fichier de script temporaire pour gnuplot
 cat <<EOF > tmp/plotscript.gnu
 set terminal pngcairo enhanced font 'Verdana,12'
 
-set output 'demo/L.png'
-set datafile separator ';'
+set output 'demo/L.png'  #fichier de sortie format png
+set datafile separator ';' #; defini comme le separateur des fichiers 
 
+#Style de l'histogramme
 set style fill solid
 set style data histograms
 set boxwidth 0.5
 set xtics rotate by -45
 
-set title "Histogramme des distances par identifiant de trajet"
-set xlabel "Identifiant du trajet"
-set ylabel "Distance (km)"
+set title "Histogramme des distances par identifiant de trajet" #titre
+set xlabel "Identifiant du trajet" #legende
+set ylabel "Distance (km)" #legende
 
+#Affichage de l'histogramme
 plot 'demo/L.txt' using 2:xtic(1) with histograms title 'Distance'
 EOF
 
-# Exécution de gnuplot avec le script créé
+#___________________________________________________________________________________________
+#                                    Derniere commande
+#___________________________________________________________________________________________
+
+#Exécution de gnuplot avec le script créé
 gnuplot tmp/plotscript.gnu
 echo "█████████▒ 90%"
 xdg-open demo/L.png
 echo "██████████ 100%"
-# Suppression du fichier de script temporaire
+#Suppression du fichier de script temporaire
 rm tmp/plotscript.gnu
 
+#Sert a compter le nombre de secondes de l'execution du programme
 end=$(echo $SECONDS );
 
+#Fais la soustraction finale pour recuperer le nombre de seconde exacte du temps d'execution
 echo "Le programme a ete execute en $(($end - $start)) secondes."
 
