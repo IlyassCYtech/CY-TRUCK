0d9d9793978cb31c2165990db3ee0b63de9262af
IlyassCYtech <130382885+IlyassCYtech@users.noreply.github.com>
2024-01-31 10:51:13 +0100

    Update S.h

--- progc/S.h
+++ progc/S.h
@@ -32,12 +32,12 @@ struct CityWithTrajets {
 };
 
 void storeCitiesInArray(struct Node *root, struct CityWithTrajets *citiesArray, int *index);
-void freeNode(struct Node *node)
-int getHeight(struct Node *N)
-int max(int a, int b)
-struct Node *newNode(struct Info ville)
-struct Node *rightRotate(struct Node *y)
-struct Node *leftRotate(struct Node *x)
-int getBalance(struct Node *N)
-struct Node *insertNode(struct Node *node, struct Info ville)
-void inorderTraversal(struct Node *root)
+void freeNode(struct Node *node);
+int getHeight(struct Node *N);
+int max(int a, int b);
+struct Node *newNode(struct Info ville);
+struct Node *rightRotate(struct Node *y);
+struct Node *leftRotate(struct Node *x);
+int getBalance(struct Node *N);
+struct Node *insertNode(struct Node *node, struct Info ville);
+void inorderTraversal(struct Node *root);
