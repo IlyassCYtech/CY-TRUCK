7b1333d127fc5721dafa137821560286fe49a248
LegrosGabriel <133345446+LegrosGabriel@users.noreply.github.com>
2024-01-31 22:10:05 +0100

    Update Help.sh
    changment

--- Help.sh
+++ Help.sh
@@ -1,38 +1,44 @@
-Vous avez deux façons de lancer le programme
-1) Si vous avez lancer le programme sans argument
-  Une interface va s'ouvrir, vous pouvez naviguer avec les fleches de votre clavier et valider l'option souhaiter avec entré
-  - Si vous choisissez Traitements
-    Entrer un numero entre 1 et 6 qui definira le nombre de traitements que vous souhaiter lancer
-    
-    
-  ùeogih
-2) Si vous avez lancer le programme avec arguments
-
-
-
-
 #!/bin/bash
-
-
 echo -e "Avec interface :"
 echo -e ""
 echo -e ""
 echo -e "Utilisation :"
 echo -e ""
-echo -e "\t🔼 Pour monter dans l'interface"
-echo -e "\t🔽 Pour descendre dans l'interface"
+echo -e "\t 🔼 Pour monter dans l'interface"
+echo -e "\t 🔽 Pour descendre dans l'interface"
+echo -e "\t Appuyer sur Entré pour validé"
 echo -e ""
 echo -e "Liste des traitements disponibles :"
 echo -e ""
-echo -e "\tTraitement -d1    |  Conducteur avec le plus de trajets       |  Temps d'execution 6 à 8 secondes"
-echo -e "\tTraitement -d2    |  Conducteurs et la plus grande disctance  |  Temps d'execution 3 à 5 secondes"  
-echo -e "\tTraitement -L     |  Les 10 trajets les plus longs            |  Temps d'execution 6 à 8 secondes"
-echo -e "\tTraitement -T     |  Les 10 villes les plus traversées        |  Temps d'execution 25 à 30 secondes"
-echo -e "\tTraitement -S     |  Statistiques sur les étapes              |  Temps d'execution 25 à 30 secondes"
-echo -e "\tTraitement -Bonus |  Conducteur avec le plus de trajet        |  Temps d'execution 26 à 30 secondes"
+echo -e "\t 🟦 Traitement -d1    |  Conducteur avec le plus de trajets       |  Temps d'execution 6 à 8 secondes"
+echo -e "\t 🟩 Traitement -d2    |  Conducteurs et la plus grande disctance  |  Temps d'execution 3 à 5 secondes"  
+echo -e "\t 🟨 Traitement -L     |  Les 10 trajets les plus longs            |  Temps d'execution 6 à 8 secondes"
+echo -e "\t 🟧 Traitement -T     |  Les 10 villes les plus traversées        |  Temps d'execution 14 à 16 secondes"
+echo -e "\t 🟥 Traitement -S     |  Statistiques sur les étapes              |  Temps d'execution 25 à 30 secondes"
+echo -e "\t 🟪 Traitement -Bonus |  Conducteur avec le plus de trajet        |  Temps d'execution 26 à 30 secondes"
+echo -e ""
+echo -e ""
+echo -e "Ajouter 5 sec en appuyant sur 'Entré'"
 
 
+echo -e ""
+echo -e ""
+echo -e ""
 
-
-
+echo -e "Sans interface :"
+echo -e ""
+echo -e ""
+echo -e "Utilisation :"
+echo -e ""
+echo -e "Ecrire './Home.sh' avec différents arguments"
+echo -e ""
+echo -e "\tPremière argument          :  Chemin ou se trouve le fichier 'data.csv'"
+echo -e "\tArguments supplémentaires  :  Indiquer les traitements souhaité"
+echo -e ""
+echo -e "\t 🟦 Pour executer traitement -d1 ajouter l'argument '-d1'"
+echo -e "\t 🟩 Pour executer traitement -d2 ajouter l'argument '-d2'"
+echo -e "\t 🟨 Pour executer traitement -l ajouter l'argument '-l'"
+echo -e "\t 🟧 Pour executer traitement -t ajouter l'argument '-t'"
+echo -e "\t 🟥 Pour executer traitement -s ajouter l'argument '-s'"
+echo -e "\t 🟪 Pour executer traitement -bonus ajouter l'argument '-bonus'"
 
