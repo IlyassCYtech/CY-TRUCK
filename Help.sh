36504f0689ae2064d3f74fb5f7ca4ebd947a3db1
LegrosGabriel <133345446+LegrosGabriel@users.noreply.github.com>
2024-01-31 20:38:29 +0100

    Update Help.sh

--- Help.sh
+++ Help.sh
@@ -12,3 +12,27 @@ temps pour chaques traitements
 
 Avec interface
 toutes les commandes et traitements
+▶️◀️🔼🔽
+echo "Avec interface"
+#!/bin/bash
+
+
+
+echo -e "Avec interface"
+echo -e "Utilisation :"
+echo -e "\t🔼 Pour monter dans l'interface"
+echo -e "\t🔽 Pour descendre dans l'interface"
+
+echo -e "Liste des traitements disponibles :"
+echo -e "\tTraitement -d1 : Description du traitement A"
+echo -e "\tTraitement -d2 : Description du traitement B"
+echo -e "\tTraitement -L : Description du traitement C"
+echo -e "\tTraitement -T : Description du traitement D"
+echo -e "\tTraitement -S : Description du traitement C"
+echo -e "\tTraitement -Bonus : Description du traitement D"
+
+
+
+
+
+
