2928948aa0bd4f260aced89e12b54f4c3721b72b
IlyassCYtech <130382885+IlyassCYtech@users.noreply.github.com>
2024-01-31 10:43:30 +0100

    Update S.c

--- progc/S.c
+++ progc/S.c
@@ -1,35 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
-#include <unistd.h>
-
-// Structure d'un nœud dans l'arbre AVL
-struct Node {
-    struct Info *ville;
-    struct Node *left;
-    struct Node *right;
-    int height;
-};
-
-// Structure des informations pour réaliser la statistique d'un noeud
-struct Info {
-    float diff;
-    int trajet;
-    float moyenne;
-    float min;
-    float max;
-};
-
-// Tableau de structure de la liste
-struct CityWithTrajets {
-    float diff;
-    int trajet;
-    float moyenne;
-    float min;
-    float max;
-    
-};
 
 // stocke les informations des villes dans un tableau
 void storeCitiesInArray(struct Node *root, struct CityWithTrajets *citiesArray, int *index) {
