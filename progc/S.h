5f180fe91a563c445874e48c5f7baf1a467beae6
IlyassCYtech <130382885+IlyassCYtech@users.noreply.github.com>
2024-01-31 10:39:21 +0100

    Update S.h

--- progc/S.h
+++ progc/S.h
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
 
+// Structure d'un nœud dans l'arbre AVL
+struct Node {
+    struct Info *ville;
+    struct Node *left;
+    struct Node *right;
+    int height;
+};
+
+// Structure des informations pour réaliser la statistique d'un noeud
+struct Info {
+    float diff;
+    int trajet;
+    float moyenne;
+    float min;
+    float max;
+};
+
+// Tableau de structure de la liste
+struct CityWithTrajets {
+    float diff;
+    int trajet;
+    float moyenne;
+    float min;
+    float max;
+    
+};
