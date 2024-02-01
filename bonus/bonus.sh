d8cefbdf97d5833ff070c1fb215f47c1c2dda4b1
IlyassCYtech <130382885+IlyassCYtech@users.noreply.github.com>
2024-02-01 20:04:26 +0100

    Update bonus.sh

--- bonus/bonus.sh
+++ bonus/bonus.sh
@@ -8,7 +8,7 @@ echo "▒▒▒▒▒▒▒▒▒▒ 0%"
 awk -F";" ' { count[$6";"$4]++ }  {count[$6";"$3]++} $6$4 == $6$3 { count[$6";"$4]-- } END { for (key in count) print key";"count[key]  }' $fichier > tmp/tmp.csv
 echo "████▒▒▒▒▒▒ 40%"
 awk -F';' '{count[$1]++} END {for (k1 in count) print k1";"count[k1]}' tmp/tmp.csv | sort -t';' -k2,2nr | head -n 10 > tmp/bonus.txt
-sort -t';' -k2 tmp/bonus.txt > demo/bonus.txt
+sort -t';' -k2 tmp/bonus.txt > tmp/tmp.txt
 
 
 gnuplot_script="tmp/gnuplot_script.txt"
@@ -18,30 +18,31 @@ echo "███████▒▒▒ 70%"
 cat << 'EQF' > "$gnuplot_script"
 
 set terminal png
-set output "demo/bonus.png"
+set output "image/bonus.png"
 
 set datafile separator ";"
 
-set title "Les 10 plus grandes distances"
+set title "Conducteur avec le plus de trajet"
 
 set yrange [0:*]      # start at zero, find max from the data
 set style fill solid  # solid color boxes
 unset key             # turn off all titles
+set xlabel 'Nb de Ville'
+set ylabel 'Chauffeur'
 
 myBoxWidth = 0.8
 set offsets 0,0,0.75-myBoxWidth/2.,0.75
 
-plot "demo/bonus.txt" using (0.5*$2):0:(0.5*$2):(myBoxWidth/2.):($0+1):ytic(1) with boxxy lc var
+plot "tmp/tmp.txt" using (0.5*$2):0:(0.5*$2):(myBoxWidth/2.):($0+1):ytic(1) with boxxy lc var
 ### end of script
 
 EQF
 echo "████████▒▒ 80%"
 #Execution du gnuplot avec le sript cree
 gnuplot "$gnuplot_script"
-xdg-open "demo/bonus.png"
+xdg-open "image/bonus.png" 
 echo "██████████ 100%"
 end=$(echo $SECONDS );
-rm "$gnuplot_script" tmp/tmp.csv tmp/bonus.txt
+rm "$gnuplot_script" tmp/tmp.csv tmp/bonus.txt tmp/tmp.txt
 echo "Le programme a ete execute en $(($end - $start)) secondes."
-
 
