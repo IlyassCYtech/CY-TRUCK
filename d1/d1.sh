08338ce577d64b2048a70e97b0541b30c34e1427
IlyassCYtech <130382885+IlyassCYtech@users.noreply.github.com>
2024-02-01 20:04:50 +0100

    Update d1.sh

--- d1/d1.sh
+++ d1/d1.sh
@@ -14,7 +14,7 @@ echo  "$premiere_liste" > tmp/premiere_liste
 liste_data=$(echo "$premiere_liste" | sort  -t';' -k2n)
 
 echo "███████▒▒▒ 70%"
-echo  "$liste_data" > demo/d1.txt
+echo  "$liste_data" > tmp/d1.txt
 
 
 #Histogramme horizontale
@@ -24,7 +24,7 @@ cat <<'EQF' > "$gnuplot_script" # debut du gnuplot_script
 
 #On s'occupe du ficher qu'on va creer, ici un png
 set terminal png
-set output "demo/d1.png"
+set output "image/d1.png"
 
 set title "Nombre de trajets par conducteur" #Titre
 
@@ -32,6 +32,8 @@ set datafile separator ';' #Separation des colonnes par des ;
 
 set yrange [0:*]      #Commence a zero, trouve le max depuis les data
 set style fill solid  #Boites solides colorees
+set xlabel 'Nb de trajet'
+set ylabel 'Chauffeur'
 unset key             #Supprime tous les titres
 
 myBoxWidth = 0.6      #Largeur des boites
@@ -40,7 +42,7 @@ set offsets 0,0,0.5-myBoxWidth/2.,0.5  # Emplacement des boites
 #myColor = "rgb '#FF0000'"
 
 #Affichage de l'histogramme par gnuplot
-plot "demo/d1.txt" using (0.5*$2):0:(0.5*$2):(myBoxWidth/2):($0+1):ytic(1) with boxxy lc var
+plot "tmp/d1.txt" using (0.5*$2):0:(0.5*$2):(myBoxWidth/2):($0+1):ytic(1) with boxxy lc var
 
 #Fin du gnuplot_script
 
@@ -50,10 +52,10 @@ EQF
 #Execution du gnuplot avec le script cree
 gnuplot "$gnuplot_script"
 echo "████████▒▒ 80%"
-xdg-open "demo/d1.png"
+xdg-open "image/d1.png"
 
 #Suppression du fichier de script temporaire
-rm "$gnuplot_script" tmp/premiere_liste
+rm "$gnuplot_script" tmp/premiere_liste tmp/d1.txt
 
 echo "██████████ 100%"
 end=$(echo $SECONDS );
